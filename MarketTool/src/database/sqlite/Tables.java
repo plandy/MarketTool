cd1d82379955c2bf836b74eba3b2cc4c3a399a48
Paul <p.landymore@gmail.com>
2016-08-25 15:12:21 +0100

    table LISTEDSTOCKS drop/create scripts

--- MarketTool/src/database/sqlite/Tables.java
+++ MarketTool/src/database/sqlite/Tables.java
@@ -2,6 +2,12 @@ package database.sqlite;
 
 public class Tables {
 	
+	public static final String DROP_LISTEDSTOCKS = "drop table if exists LISTEDSTOCKS";
+	public static final String CREATE_LISTEDSTOCKS = "create table LISTEDSTOCKS "
+			+ "(TICKER string not null, "
+			+ "FULLNAME string not null, "
+			+ "primary key (TICKER) )";
+	
 	public static final String DROP_PRICEHISTORY = "drop table if exists PRICEHISTORY ";
 	public static final String CREATE_PRICEHISTORY = "create table PRICEHISTORY "
 			+ "(TICKER string not null, "
