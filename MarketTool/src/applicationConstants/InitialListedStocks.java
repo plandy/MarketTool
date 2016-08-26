63ecd4040e15fe1b0c59c9eb0d61c41dc3f2d674
Paul <p.landymore@gmail.com>
2016-08-26 18:39:16 +0100

    Adding to initial list of stocks

--- MarketTool/src/applicationConstants/InitialListedStocks.java
+++ MarketTool/src/applicationConstants/InitialListedStocks.java
@@ -1,7 +1,6 @@
 package applicationConstants;
 
 import java.util.ArrayList;
-
 import priceHistory.ListedStockTO;
 
 public class InitialListedStocks {
@@ -11,7 +10,22 @@ public class InitialListedStocks {
 	static {
 		listedStocks = new ArrayList<ListedStockTO>();
 		
+		listedStocks.add( new ListedStockTO("AAPL", "Apple Inc.") );
+		listedStocks.add( new ListedStockTO("BABA", "Alibaba Group Holding Limited") );
+		listedStocks.add( new ListedStockTO("BAC", "Bank pf America Corporation") );
+		listedStocks.add( new ListedStockTO("C", "Citigroup Inc.") );
+		listedStocks.add( new ListedStockTO("F", "Ford Motor Co.") );
+		listedStocks.add( new ListedStockTO("FB", "Facebook, Inc.") );
+		listedStocks.add( new ListedStockTO("GE", "General Electric Company") );
+		listedStocks.add( new ListedStockTO("GM", "General Motors Company") );
+		listedStocks.add( new ListedStockTO("HPQ", "HP Inc.") );
 		listedStocks.add( new ListedStockTO("IBM", "International Business Machines Corporation") );
+		listedStocks.add( new ListedStockTO("JPM", "JPMorgan Chase & Co.") );
+		listedStocks.add( new ListedStockTO("MRO", "Marathon Oil Corporation") );
+		listedStocks.add( new ListedStockTO("MSFT", "Microsoft Corporation") );
+		listedStocks.add( new ListedStockTO("PFE", "Pfizer Inc.") );
+		listedStocks.add( new ListedStockTO("PG", "The Procter & Gamble Company") );
+		listedStocks.add( new ListedStockTO("VZ", "Verizon Communications Inc.") );
 	}
 
 }
--- MarketTool/src/priceHistory/ListedStockTO.java
+++ MarketTool/src/priceHistory/ListedStockTO.java
@@ -2,12 +2,25 @@ package priceHistory;
 
 public class ListedStockTO {
 	
-	public String ticker;
-	public String fullName;
+	private String ticker;
+	private String fullName;
 	
 	public ListedStockTO( String p_ticker, String p_fullName ) {
 		ticker = p_ticker;
 		fullName = p_fullName;
+	}
+	
+	@Override
+	public String toString() {
+		return ticker;
+	}
+	
+	public String getTicker() {
+		return ticker;
+	}
+	
+	public String getFullname() {
+		return fullName;
 	}
 	
 }
