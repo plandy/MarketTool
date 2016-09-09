5baeac727264d46d26c11fb685a257c4b6d8ee68
Paul <p.landymore@gmail.com>
2016-09-09 21:08:05 +0100

    removing now unused methods from PriceHistoryController

--- MarketTool/src/priceHistory/PriceHistoryController.java
+++ MarketTool/src/priceHistory/PriceHistoryController.java
@@ -20,42 +20,6 @@ public class PriceHistoryController {
 		view = p_view;
 	}
 	
-	public XYChart.Series<Date, Number> getPriceChartData( String p_ticker ) {
-		XYChart.Series<Date, Number> closePriceSeries = new XYChart.Series<>();
-		
-		Date todayDate = DateUtility.getTodayDate();
-		Date beginDate = DateUtility.parseStringToDate("2015-08-29");
-		
-		List<DataFeedTO> priceHistory = service.getPriceChartData( p_ticker, beginDate, todayDate );
-		Date l_thisDate;
-		
-		for ( DataFeedTO dataTO : priceHistory ) {
-			l_thisDate = DateUtility.parseStringToDate( dataTO.getDate() );
-			Data<Date, Number> data = new Data<Date, Number>( l_thisDate, (Number)dataTO.getClosePrice() );
-			
-			closePriceSeries.getData().add( data );
-		}
-
-		return closePriceSeries;
-	}
-	
-	public XYChart.Series<String, Number> getVolumeChartData( String p_ticker ) {
-		XYChart.Series<String, Number> volumeSeries = new XYChart.Series<>();
-		
-		Date todayDate = DateUtility.getTodayDate();
-		Date beginDate = DateUtility.parseStringToDate("2015-08-29");
-		
-		List<DataFeedTO> priceHistory = service.getPriceChartData( p_ticker, beginDate, todayDate );
-		
-		for ( DataFeedTO dataTO : priceHistory ) {
-			Data<String, Number> data = new Data<String, Number>( dataTO.getDate(), (Number)(dataTO.getVolume()/100000) );
-			
-			volumeSeries.getData().add( data );
-		}
-
-		return volumeSeries;
-	}
-	
 	public ObservableList<ListedStockTO> getListedStocks() {
 		List<ListedStockTO> listedStocks = service.getListedStocks();		
 		return FXCollections.observableArrayList( listedStocks );
